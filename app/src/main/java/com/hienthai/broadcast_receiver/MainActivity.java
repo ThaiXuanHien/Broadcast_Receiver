d2d3f84ad109b90b6ceab44e58f4e8f980b983fe
hienthai <xtit6789@gmail.com>
2021-06-23 11:27:44 +0700

    custom Broadcast

--- app/src/main/java/com/hienthai/broadcast_receiver/MainActivity.java
+++ app/src/main/java/com/hienthai/broadcast_receiver/MainActivity.java
@@ -2,33 +2,89 @@ package com.hienthai.broadcast_receiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private InternetBroadcast internetBroadcast;
+    public static final String MY_ACTION = "MY_ACTION";
+    public static final String KEY_DATA = "MY_DATA";
+    public static final String DATA = "DATA_THAI_XUAN_HIEN";
+
+    // private InternetBroadcast internetBroadcast; Part 1
+
+    BroadcastReceiver myBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (MY_ACTION.equals(intent.getAction())) {
+                String data = intent.getStringExtra(KEY_DATA);
+                textView.setText(data);
+            }
+        }
+    };
+
+    TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        internetBroadcast = new InternetBroadcast();
+        anhXa();
+
+        // internetBroadcast = new InternetBroadcast(); Part 1
+
+
+        button.setOnClickListener(v -> {
+            clickSendBroadcast();
+        });
     }
+
+    private void clickSendBroadcast() {
+        Intent intent = new Intent(MY_ACTION);
+        intent.putExtra(KEY_DATA, DATA);
+        sendBroadcast(intent);
+
+    }
+
+    private void anhXa() {
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+    }
+
+//    Part 1
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//        registerReceiver(internetBroadcast, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//
+//        unregisterReceiver(internetBroadcast);
+//    }
+
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        registerReceiver(internetBroadcast, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        registerReceiver(myBroadcastReceiver, new IntentFilter(MY_ACTION));
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-
-        unregisterReceiver(internetBroadcast);
+        unregisterReceiver(myBroadcastReceiver);
     }
 }
