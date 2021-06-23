fe5a32cb9770d33a417b5bdf3f3c2f5ee5543dd4
hienthai <xtit6789@gmail.com>
2021-06-23 13:14:58 +0700

    send list user

--- app/src/main/java/com/hienthai/broadcast_receiver/MainActivity.java
+++ app/src/main/java/com/hienthai/broadcast_receiver/MainActivity.java
@@ -11,6 +11,16 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String MY_ACTION = "MY_ACTION";
@@ -24,10 +34,35 @@ public class MainActivity extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             if (MY_ACTION.equals(intent.getAction())) {
                 String data = intent.getStringExtra(KEY_DATA);
-                textView.setText(data);
+
+                List<User> users = getListUser(data);
+                String result = "";
+                for (int i = 0; i < users.size(); i++) {
+                    result += users.get(i).getId() + " _ " + users.get(i).getName() + "\n";
+                }
+
+                textView.setText(result);
             }
         }
     };
+
+    private List<User> getListUser(String data) {
+        List<User> userList = new ArrayList<>();
+        try {
+            JSONArray jsonArray = new JSONArray(data);
+            JSONObject jsonObject;
+            User user;
+            Gson gson = new Gson();
+            for (int i = 0; i < jsonArray.length(); i++) {
+                jsonObject = jsonArray.getJSONObject(i);
+                user = gson.fromJson(jsonObject.toString(), User.class);
+                userList.add(user);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return userList;
+    }
 
     TextView textView;
     Button button;
@@ -49,7 +84,18 @@ public class MainActivity extends AppCompatActivity {
 
     private void clickSendBroadcast() {
         Intent intent = new Intent(MY_ACTION);
-        intent.putExtra(KEY_DATA, DATA);
+
+        User user1 = new User(1, "Hien");
+        User user2 = new User(2, "Tra My");
+        List<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+
+        Gson gson = new Gson();
+        JsonArray jsonArray = gson.toJsonTree(users).getAsJsonArray();
+        String strJson = jsonArray.toString();
+
+        intent.putExtra(KEY_DATA, strJson);
         sendBroadcast(intent);
 
     }
